5c90a515523087128a4c851225159e7edaa0aaf4
muhammedirshad1 <muhammed.irshad@thinkpalm.com>
2023-01-06 14:54:22 +0530

    GetCustomer

--- TestNinja.UnitTest/CustomerControllerTests.cs
+++ TestNinja.UnitTest/CustomerControllerTests.cs
@@ -19,5 +19,15 @@ namespace TestNinja.UnitTest
             Assert.That(result,Is.TypeOf<NotFound>());
             Assert.That(result,Is.InstanceOf<NotFound>());
         } 
+
+        [Test]
+        public void GetCustomer_idIsNotZero_ReturnsOk(){
+            var customer = new CustomerController();
+
+            var result = customer.GetCustomer(1);
+
+            Assert.That(result,Is.InstanceOf<Ok>());
+            Assert.That(result,Is.TypeOf<Ok>());
+        }
     }
 }
