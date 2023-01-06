baff63db89275c4485b4cb5be54576486814f6e5
muhammedirshad1 <muhammed.irshad@thinkpalm.com>
2023-01-06 14:49:39 +0530

    CustomerControllerTests

---
+++ TestNinja.UnitTest/CustomerControllerTests.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using TestNinja5.Fundamentals;
+
+namespace TestNinja.UnitTest
+{
+    [TestFixture]
+    public class CustomerControllerTests
+    {
+        [Test]
+        public void GetCustomer_idIsZero_ReturnsNotFound(){
+            var customer = new CustomerController();
+
+            var result = customer.GetCustomer(0);
+
+            Assert.That(result,Is.TypeOf<NotFound>());
+            Assert.That(result,Is.InstanceOf<NotFound>());
+        } 
+    }
+}
